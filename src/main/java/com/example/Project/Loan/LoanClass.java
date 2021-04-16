de7129388b92c58a6e8989d1dfeb9953a7ee10e7
Sreekums <sreekums96@gmail.com>
2021-04-16 21:18:01 +0530

    Increased Parameter count to 15

--- src/main/java/com/example/Project/Loan/LoanClass.java
+++ src/main/java/com/example/Project/Loan/LoanClass.java
@@ -11,23 +11,54 @@ import javax.persistence.*;
 	private long id;
 	private String name;
 	private long loancapital;
-
-    public LoanClass() {}
+	private int period;
+	private boolean employ;
+	private long credscore;
+	private long empolyexp;
+	private boolean emistat;
+	private String dob;
+	private int def_days;
+	private boolean public_sector;
+	private int profit_proj;
+	private boolean crim_bg;
+	private boolean ec_off;
+	private String collateral_type;
+	private String ltype;
 	
-	public LoanClass(long id, String name, long capital) {        //Constructor which will instantiate the created objects
-		super();
+	
+
+    public LoanClass() {}   
+	
+	public LoanClass(long id, String name, long loancapital, int period, boolean employ, long credscore, long empolyexp,
+			boolean emistat, String dob, int def_days, boolean public_sector, int profit_proj, boolean crim_bg,
+			boolean ec_off, String collateral_type, String ltype) 
+	{
 		this.id = id;
 		this.name = name;
-		this.loancapital = capital;
+		this.loancapital = loancapital;
+		this.period = period;
+		this.employ = employ;                                             //Constructor which will instantiate the created objects
+		this.credscore = credscore;
+		this.empolyexp = empolyexp;
+		this.emistat = emistat;
+		this.dob = dob;
+		this.def_days = def_days;
+		this.public_sector = public_sector;
+		this.profit_proj = profit_proj;
+		this.crim_bg = crim_bg;
+		this.ec_off = ec_off;
+		this.collateral_type = collateral_type;
+		this.ltype = ltype;
 	}
 
 	public long getId() {
 		return id;
-	}	
+	}
 
 	public void setId(long id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
@@ -35,12 +66,120 @@ import javax.persistence.*;
 	public void setName(String name) {
 		this.name = name;
 	}
-	public long getCapital() {
+
+	public long getLoancapital() {
 		return loancapital;
 	}
-	public void setCapital(long capital) {
-		this.loancapital = capital;
+
+	public void setLoancapital(long loancapital) {
+		this.loancapital = loancapital;
 	}
+
+	public int getPeriod() {
+		return period;
+	}
+
+	public void setPeriod(int period) {
+		this.period = period;
+	}
+
+	public boolean isEmploy() {
+		return employ;
+	}
+
+	public void setEmploy(boolean employ) {
+		this.employ = employ;
+	}
+
+	public long getCredscore() {
+		return credscore;
+	}
+
+	public void setCredscore(long credscore) {
+		this.credscore = credscore;
+	}
+
+	public long getEmpolyexp() {
+		return empolyexp;
+	}
+
+	public void setEmpolyexp(long empolyexp) {
+		this.empolyexp = empolyexp;
+	}
+
+	public boolean isEmistat() {
+		return emistat;
+	}
+
+	public void setEmistat(boolean emistat) {
+		this.emistat = emistat;
+	}
+
+	public String getDob() {
+		return dob;
+	}
+
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+
+	public int getDef_days() {
+		return def_days;
+	}
+
+	public void setDef_days(int def_days) {
+		this.def_days = def_days;
+	}
+
+	public boolean isPublic_sector() {
+		return public_sector;
+	}
+
+	public void setPublic_sector(boolean public_sector) {
+		this.public_sector = public_sector;
+	}
+
+	public int getProfit_proj() {
+		return profit_proj;
+	}
+
+	public void setProfit_proj(int profit_proj) {
+		this.profit_proj = profit_proj;
+	}
+
+	public boolean isCrim_bg() {
+		return crim_bg;
+	}
+
+	public void setCrim_bg(boolean crim_bg) {
+		this.crim_bg = crim_bg;
+	}
+
+	public boolean isEc_off() {
+		return ec_off;
+	}
+
+	public void setEc_off(boolean ec_off) {
+		this.ec_off = ec_off;
+	}
+
+	public String getCollateral_type() {
+		return collateral_type;
+	}
+
+	public void setCollateral_type(String collateral_type) {
+		this.collateral_type = collateral_type;
+	}
+
+	public String getLtype() {
+		return ltype;
+	}
+
+	public void setLtype(String ltype) {
+		this.ltype = ltype;
+	}
+
+	
 	
 
 }
--- src/main/java/com/example/Project/Loan/LoanFileController.java
+++ src/main/java/com/example/Project/Loan/LoanFileController.java
@@ -1,14 +1,18 @@
 package com.example.Project.Loan;
 
+import java.util.Date;
 import java.util.List;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import com.sun.el.parser.ParseException;
 
 @RestController 
 public class LoanFileController {               //Loan File Controller handles the REST APIs to the application 
@@ -20,7 +24,7 @@ public class LoanFileController {               //Loan File Controller handles t
 	}
 
 	@RequestMapping(value="/loadFile", method=RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-	public String fileUpload(@RequestParam("file") MultipartFile file) throws IOException
+	public String fileUpload(@RequestParam("file") MultipartFile file) throws IOException,ParseException
 	{
 		File rec=new File("H:\\"+file.getOriginalFilename());
 		BufferedReader br=new BufferedReader(new FileReader(rec));            //fileUpload() method handles the POST request
@@ -28,8 +32,23 @@ public class LoanFileController {               //Loan File Controller handles t
 		while((line = br.readLine())!=null)                                   //to extract the data
 		{
 			String[] parse = line.split(",");
-			LoanClass r= new LoanClass(Long.parseLong(parse[0]),parse[1],Long.parseLong(parse[2]));
-			System.out.println(r.getId()+" "+r.getName()+" "+r.getCapital());
+			LoanClass r= new LoanClass(Long.parseLong(parse[0]),
+					parse[1],
+					Long.parseLong(parse[2]),
+					Integer.parseInt(parse[3]),
+					Boolean.parseBoolean(parse[4]),
+					Long.parseLong(parse[5]),
+					Long.parseLong(parse[6]),
+					Boolean.parseBoolean(parse[7]),
+					parse[8],
+					Integer.parseInt(parse[9]),
+					Boolean.parseBoolean(parse[10]),
+					Integer.parseInt(parse[11]),
+					Boolean.parseBoolean(parse[12]),
+					Boolean.parseBoolean(parse[13]),
+					parse[14],
+					parse[15]);
+			//System.out.println(r.getId()+" "+r.getName()+" "+r.getCapital());
 			if(ls.loanSelection())                                               //loanSelection() method is responsible for implement business logic from the LoanService object to reject loan randomly 
 			{
 				ls.saveLoan(r);                                                  //saveLoan() method is responsible for saving the accepted loans to the database using LoanService
